368bb608159b17e513d5829c877097d82efbd1f8
Harsh Rai <htc38199@gmail.com>
2025-01-10 20:21:37 +0530

    feat:added job scheduler for fetching data preodically

---
+++ jobs/fetchCryptoData.js
@@ -0,0 +1,29 @@
+import { CronJob } from 'cron';
+import axios from 'axios';
+import Crypto from '../models/cryptoModel.js';
+
+const fetchCryptoData = async () => {
+    const coins = ['bitcoin', 'matic-network', 'ethereum'];
+    try {
+        for (const coin of coins) {
+            const response = await axios.get(`https://api.coingecko.com/api/v3/simple/price?ids=${coin}&vs_currencies=usd&include_market_cap=true&include_24hr_change=true`);
+            const data = response.data[coin];
+            console.log(`Fetched data for ${coin}:`, data);
+            const crypto = new Crypto({
+                coin,
+                price: data.usd,
+                marketCap: data.usd_market_cap,
+                change24h: data.usd_24h_change
+            });
+
+            await crypto.save();
+            console.log(`Data for ${coin} saved.`);
+        }
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+};
+
+const job = new CronJob('*/5 * * * *', fetchCryptoData, null, true, 'UTC');
+
+job.start();
--- server.js
+++ server.js
@@ -1,8 +1,33 @@
-import express from "express";
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import startCryptoDataJob from './jobs/fetchCryptoData.js';
+import cryptoRoutes from './routes/cryptoRoutes.js';
+
+dotenv.config({ path: './config.env' });
 
 const app = express();
-const port = 8080
+const PORT = process.env.PORT || 3000;
 
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`)
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
+.then(() => {
+    console.log('Connected to MongoDB');
+    
+    startCryptoDataJob();
+})
+.catch((error) => {
+    console.error('MongoDB connection error:', error);
+});
+
+app.use('/api/crypto', cryptoRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
